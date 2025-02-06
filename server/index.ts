a21c9ab6ec790667262c826f37716673bc1d4f52
Сергей Зенин <sergey.zenin@kaspersky.com>
2025-02-06 22:10:41 +0300

    feat: update ws logic on server

--- server/index.ts
+++ server/index.ts
@@ -5,6 +5,7 @@ import { dishes } from "./dishes";
 import { menu } from "./menu";
 
 const server = fastify(({ logger: true }))
+let count = 0
 
 server.get('/dishes', async () => {
   return dishes
@@ -14,14 +15,55 @@ server.get('/menu', async () => {
   return menu
 })
 
+server.get('/count', async () => {
+  return count
+})
+
+const getRandomNumber = () => Math.round(Math.random() * 4 + 1)
+
+
 const start = async () => {
   try {
     server.register(fastifyWebsocket)
-
     server.register(async function (fastify) {
       fastify.get('/', { websocket: true }, socket => {
-        socket.on('message', () => {
-          socket.send('hi from server')
+        socket.on('message', async event => {
+          const { action, reqId } = JSON.parse(event.toString())
+         
+          if (action === 'plus') {
+            count += 1
+            socket.send(JSON.stringify({ reqId, result: 'ok' }))
+            return
+          }
+
+          if (action === 'minus') {
+            count -= 1
+            socket.send(JSON.stringify({ reqId, result: 'ok' }))
+            return
+          }
+
+          if (action === 'feature1:get-apples') {
+            await new Promise(resolve => setTimeout(() => resolve(0), 1000))
+            socket.send(JSON.stringify({ reqId, action: `${action}:result`, result: getRandomNumber() }))
+            return
+          }
+
+          if (action === 'feature1:get-pears') {
+            await new Promise(resolve => setTimeout(() => resolve(0), 1000))
+            socket.send(JSON.stringify({ reqId, action: `${action}:result`, result: getRandomNumber() }))
+            return
+          }
+
+          if (action === 'feature2:get-fruits') {
+            await new Promise(resolve => setTimeout(() => resolve(0), 1000))
+            socket.send(JSON.stringify({ 
+              reqId,
+              action: `${action}:result`,
+              result: {
+                apples: getRandomNumber(),
+                pears: getRandomNumber()
+              } }))
+          }
         })
       })
     })
